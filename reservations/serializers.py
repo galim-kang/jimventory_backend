f602a09e938650daacd8add5ba340d60b1fb9252
galim-kang <togarim@naver.com>
2024-01-23 14:03:09 +0900

    get reseInclude storage information in get reservations requests.rvations

--- reservations/serializers.py
+++ reservations/serializers.py
@@ -1,10 +1,15 @@
-# reservations/serializers.py
-
 from rest_framework import serializers
 from .models import Reservation
+from storages.models import Storage
+
+class StorageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Storage
+        fields = ['hostUser', 'storeType', 'serviceName', 'address', 'latitude', 'longitude', 'operatingTime', 'description', 'introduction', 'available', 'image', 'contact_info']
 
 class ReservationSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    storage = StorageSerializer()  # Storage에 대한 정보를 포함
 
     class Meta:
         model = Reservation
